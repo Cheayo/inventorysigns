c94bcf698d67330f22f760096462ff018bc8ad62
Lessa Winston <lessa.winston@yahoo.com>
2015-12-11 13:07:12 -0800

    Update inventorysigns.rb
    Fixed /**/ comments to #
    Too much CSS lately I guess

--- inventorysigns.rb
+++ inventorysigns.rb
@@ -1,29 +1,29 @@
-require 'rubyXL' /*for processing Excel file*/
+require 'rubyXL' # for processing Excel file
 require 'rubygems'
-require 'rtf'   /*makes rtf document*/
-require 'barby' /*makes barcodes*/
+require 'rtf'   # makes rtf document
+require 'barby' # makes barcodes
 require 'barby/barcode/code_128'
 require 'barby/outputter/png_outputter'
 
 include RTF
 
-xls = RubyXL::Parser.parse(ARGV[0])  /*accepts the source Excel file*/
+xls = RubyXL::Parser.parse(ARGV[0])  # accepts the source Excel file
 data = xls.worksheets[0]
 
 document = Document.new(Font.new(Font::ROMAN, 'Arial'))
 
 i = 1
 lines = 0
-while defined?(data.sheet_data[i][4].value)  /*breakdown of Excel file*/
-	copies = data.sheet_data[i][4].value  /*number of inventory signs varies*/
+while defined?(data.sheet_data[i][4].value)  # breakdown of Excel file
+	copies = data.sheet_data[i][4].value  # number of inventory signs varies
 		
-	barcode = Barby::Code128B.new(data.sheet_data[i][3].value) /*converts data to code 128 barcode */
+	barcode = Barby::Code128B.new(data.sheet_data[i][3].value) # converts data to code 128 barcode 
 	blob = Barby::PngOutputter.new(barcode)
 	blob.height = 50
 	blob.xdim = 2
-	File.open('barcode.png', 'wb') {|f| f.write blob.to_png}  /*barcode is png file*/
+	File.open('barcode.png', 'wb') {|f| f.write blob.to_png}  # barcode is png file
 	
-	copies.times do  /*builds this label and adds it to the rtf file*/ 
+	copies.times do  # builds this label and adds it to the rtf file 
 		document.paragraph do |p| 
 			p << "QMI\t\tMARKER"
 			p.line_break
@@ -47,7 +47,7 @@ while defined?(data.sheet_data[i][4].value)  /*breakdown of Excel file*/
 	i += 1
 end
 
-File.open('inventorylabels.rtf', 'w+') {|file| file.write(document.to_rtf)} /*write to the rtf file*/
+File.open('inventorylabels.rtf', 'w+') {|file| file.write(document.to_rtf)} # write to the rtf file
 
-ObjectSpace.each_object(IO) {|f| f.close unless f.closed? } /*close any and all instances of the png so it can be deleted*/
+ObjectSpace.each_object(IO) {|f| f.close unless f.closed? } # close any and all instances of the png so it can be deleted
 File.delete('barcode.png')
